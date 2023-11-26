52c98cc663d3cd0920f027a3fd06d3155e8a4c2f
[vdtkk] <vdtkk@hotmail.com>
2023-11-26 23:01:41 +0300

    home oluştruld

---
+++ src/home.vue
@@ -0,0 +1,37 @@
+<template>
+   <div>
+    <p> LAMBA DURUMU :   
+    <button @click="openLamp"> Lambayı aç/Kapa {{sta }}</button></p>
+   </div>
+ 
+
+</template>
+
+<script>
+import func from "../vue-temp/vue-editor-bridge"
+  
+
+export default defineComponent({
+    data() {
+        
+        return {
+            status: "kapalı"
+        }
+    },
+    methods: {
+
+        openLamp(){
+            this.status="acik.."
+
+        }
+
+    }
+
+
+})
+</script>
+
+
+<style >
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from "./Home"
+
+vue.component("lamp-component",Home);
+
+
 
 new Vue({
   el: '#app',
+
+
   render: h => h(App)
 })
